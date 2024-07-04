f2be9cd0b53bff97fcc4c2986d7282b5b99638db
irfandi88 <166503127+irfandi88@users.noreply.github.com>
2024-07-04 22:33:05 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,92 @@
+import 'package:aplikasi_pendataan_alat_tulis_kantor/admin_dashboard_page.dart';
+import 'package:aplikasi_pendataan_alat_tulis_kantor/admin_pages/admin_home_page.dart';
+import 'package:aplikasi_pendataan_alat_tulis_kantor/admin_pages/admin_profile_page.dart';
+import 'package:aplikasi_pendataan_alat_tulis_kantor/admin_pages/admin_search_page.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/contact.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/dashboard_page.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/forgot_password_page.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/login_page.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/pengambilan_barang_atk.dart';
+import 'package:aplikasi_pendataan_alat_tulis_kantor/pengambilan_barang_atk_form.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/permintaan_page.dart';
-import 'package:aplikasi_pendataan_alat_tulis_kantor/permintaan_page_2.dart';
+import 'package:aplikasi_pendataan_alat_tulis_kantor/permintaan_page_form.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/persediaan_barang_atk_page.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/profile.dart';
+import 'package:aplikasi_pendataan_alat_tulis_kantor/providers/permintaan_view_model.dart';
+import 'package:aplikasi_pendataan_alat_tulis_kantor/providers/persediaan_view_model.dart';
+import 'package:aplikasi_pendataan_alat_tulis_kantor/providers/view_model.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/register_page.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/tambah_barang_atk_admin.dart';
 import 'package:aplikasi_pendataan_alat_tulis_kantor/tambah_item_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RequestViewModel()),
+        ChangeNotifierProvider(create: (_) => PermintaanViewModel()),
+        ChangeNotifierProvider(create: (_) => PersediaanViewModel()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ATK Application',
       initialRoute: '/',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       routes: {
-        '/': (context) => HomePage(),
-        '/register': (context) => RegisterPage(),
-        '/login': (context) => LoginPage(),
-        '/forgotPassword': (context) => ForgotPasswordPage(),
+        '/': (context) => const HomePage(),
+        '/register': (context) => const RegisterPage(),
+        '/login': (context) => const LoginPage(),
+        '/forgotPassword': (context) => const ForgotPasswordPage(),
+        '/adminHome': (context) => AdminDashboardHomePage(),
+        '/adminProfile': (context) => AdminDashboardProfilePage(),
+        '/adminSearch': (context) => AdminSearchPage(),
+        '/adminDashboard': (context) => AdminDashboardPage(),
         '/dashboard': (context) => DashboardPage(),
-        '/persediaanBarang': (context) => PersediaanBarangATKPage(),
-        '/tambahItem': (context) => TambahItemATKPage(),
-        '/tambahBarangAdmin': (context) => TambahItemATKPageAdmin(),
-        '/permintaanBarang': (context) => PermintaanPage(),
-        '/permintaanBarang2': (context) => PermintaanBarangDuaPage(),
-        '/pengambilanBarang': (context) => PengambilanBarang(),
-        '/contact': (context) => ContactPage(),
-        '/profile': (context) => ProfilePage(),
+        '/persediaanBarang': (context) =>
+            const PersediaanBarangATKPage(items: []),
+        '/tambahItem': (context) => const TambahItemATKPage(
+              itemsv1: [],
+            ),
+        '/tambahBarangAdmin': (context) =>
+            const TambahItemATKPageAdmin(itemsv1: []),
+        '/permintaanBarang': (context) => const PermintaanPage(
+              items: [],
+            ),
+        '/permintaanBarangForm': (context) => const PermintaanBarangForm(),
+        '/pengambilanBarang': (context) => const PengambilanBarang(
+              items: [],
+            ),
+        '/pengambilanBarangForm': (context) => const PengambilanBarangForm(
+              itemsv1: [],
+            ),
+        '/contact': (context) => const ContactPage(),
+        '/profile': (context) => const ProfilePage(),
       },
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.withOpacity(0.2),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -127,16 +167,6 @@ class HomePage extends StatelessWidget {
                   style: TextStyle(color: Colors.blue)),
             ),
             const SizedBox(height: 20),
-            Container(
-              height: 100,
-              width: 300,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2.0,
-                  )),
-            ),
           ],
         ),
       ),
