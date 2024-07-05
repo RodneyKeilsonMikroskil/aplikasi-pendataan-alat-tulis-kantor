cf3c18fea1d0e265b98b41fa1dd4819af66f0911
RodneyKeilsonMikroskil <166992884+RodneyKeilsonMikroskil@users.noreply.github.com>
2024-07-05 18:15:21 +0700

    dashboard bottom nav bar contents

---
+++ lib/dashboard_pages/profile_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class DashboardProfilePage extends StatelessWidget {
+  const DashboardProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: false,),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset(
+            'logo-color.png',
+            height: 150,
+            width: 185,
+            fit: BoxFit.contain,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Aplikasi Pendataan\nATK (Alat Tulis Kantor)',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            height: 1.0,
+            width: 250,
+            color: Colors.blue,
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Profile',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 1.0,
+            width: 325,
+            color: Colors.blue,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text("Coming soon."),
+          const SizedBox(
+            height: 20,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ]),
+      ),
+    );
+  }
+}
